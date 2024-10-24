49d7f8dc84abd5d1fc46ae22f1bef4d8c4da508b
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-10-24 14:18:56 +0300

    add validation constants and error massage on BG

---
+++ ElProApp/ElProApp.Common/EntityValidationConstants.cs
@@ -0,0 +1,32 @@
+﻿namespace ElProApp.Common
+{    
+    public static class EntityValidationConstants
+    {
+        public static class Employee
+        {
+            public const int NameMinLength = 2;
+            public const int NameMaxLength = 20;
+        }
+
+        public static class Building
+        {
+            public const int BuildingNameMinLength = 3;
+            public const int BuildingNameMaxLength = 50;
+            public const int LocationMinLength = 10;
+            public const int LocationMaxLength = 100;
+        }
+
+        public static class Team
+        {
+            public const int NameMinLength = 5;
+            public const int NameMaxLength = 100;
+        }
+
+        public static class Job
+        {
+            public const int nameMinLength = 5;
+            public const int nameMaxLength = 50;
+        }
+
+    }
+}
