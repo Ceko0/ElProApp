1227ae5dc8c76faf3f531dff1bb74b8cd28bc2f2
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-11-06 18:02:46 +0200

    adding seed data for mapping entity

--- ElProApp/ElProApp.Data/SeedData/DatabaseSeeder.cs
+++ ElProApp/ElProApp.Data/SeedData/DatabaseSeeder.cs
@@ -1,29 +1,33 @@
 ﻿namespace ElProApp.Data.SeedData
 {
+    using ElProApp.Data.Models.Mappings;
     using Microsoft.AspNetCore.Identity;
     using Newtonsoft.Json;
     using System.IO;
-    using System.Linq; // Не забравяйте да добавите this за LINQ операции
+    using System.Linq;
 
-    public class DatabaseSeeder
+    public class DatabaseSeeder(ElProAppDbContext context, UserManager<IdentityUser> userManager)
     {
-        private readonly ElProAppDbContext context;
-        private readonly UserManager<IdentityUser> userManager;
-
-        public DatabaseSeeder(ElProAppDbContext context, UserManager<IdentityUser> userManager)
-        {
-            this.context = context;
-            this.userManager = userManager;
-        }
+        private readonly ElProAppDbContext context = context;
+        private readonly UserManager<IdentityUser> userManager = userManager;
 
         public void SeedDatabase()
+        {
+            SeedInitialData();
+            SeedMappingData();
+        }
+
+        private void SeedInitialData()
         {
             var jsonData = File.ReadAllText("D:\\ProgramsScool\\ElProApp\\ElProApp\\ElProApp.Data\\SeedData\\SeedData.json");
             var initialData = JsonConvert.DeserializeObject<InitialData>(jsonData);
 
             foreach (var team in initialData.Teams)
             {
-                if (!context.Teams.Any(x => x.Id == team.Id)) context.Teams.Add(team);                
+                if (!context.Teams.Any(x => x.Id == team.Id))
+                {
+                    context.Teams.Add(team);
+                }
             }
             context.SaveChanges();
 
@@ -43,11 +47,19 @@
 
                     var result = userManager.CreateAsync(user, "123456").Result;
 
-                    if (result.Succeeded) context.Employees.Add(employee);                    
-                    else throw new Exception($"Failed to create user: {string.Join(", ", result.Errors.Select(e => e.Description))}");
-                    
+                    if (result.Succeeded)
+                    {
+                        context.Employees.Add(employee);
+                    }
+                    else
+                    {
+                        throw new Exception($"Failed to create user: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+                    }
                 }
-                else Console.WriteLine($"User with ID {employee.UserId} already exists.");                
+                else
+                {
+                    Console.WriteLine($"User with ID {employee.UserId} already exists.");
+                }
             }
             context.SaveChanges();
 
@@ -60,18 +72,75 @@
             }
             context.SaveChanges();
 
-
             foreach (var job in initialData.Jobs)
             {
-                if (!context.Jobs.Any(x => x.Id == job.Id)) context.Jobs.Add(job);                
+                if (!context.Jobs.Any(x => x.Id == job.Id))
+                {
+                    context.Jobs.Add(job);
+                }
             }
             context.SaveChanges();
 
-
             foreach (var jobDone in initialData.JobDones)
             {
-                if (!context.JobsDone.Any(x => x.Id == jobDone.Id)) context.JobsDone.Add(jobDone);                
+                if (!context.JobsDone.Any(x => x.Id == jobDone.Id))
+                {
+                    context.JobsDone.Add(jobDone);
+                }
             }
+            context.SaveChanges();
+        }
+
+        private void SeedMappingData()
+        {
+            var jsonData = File.ReadAllText("D:\\ProgramsScool\\ElProApp\\ElProApp\\ElProApp.Data\\SeedData\\MappingSeedData.json");
+            var mappingData = JsonConvert.DeserializeObject<MappingData>(jsonData);
+
+            foreach (var mapping in mappingData.Mappings)
+            {
+                // Проверка и добавяне на мапинг на BuildingTeamMapping
+                foreach (var buildingId in mapping.BuildingIds)
+                {
+                    if (!context.BuildingTeamMappings.Any(x => x.BuildingId == buildingId && x.TeamId == mapping.TeamId))
+                    {
+                        var buildingTeamMapping = new BuildingTeamMapping
+                        {
+                            BuildingId = buildingId,
+                            TeamId = mapping.TeamId
+                        };
+                        context.BuildingTeamMappings.Add(buildingTeamMapping);
+                    }
+                }
+
+                // Проверка и добавяне на мапинг на EmployeeTeamMapping
+                foreach (var employeeId in mapping.EmployeeIds)
+                {
+                    if (!context.EmployeeTeamMappings.Any(x => x.EmployeeId == employeeId && x.TeamId == mapping.TeamId))
+                    {
+                        var employeeTeamMapping = new EmployeeTeamMapping
+                        {
+                            EmployeeId = employeeId,
+                            TeamId = mapping.TeamId
+                        };
+                        context.EmployeeTeamMappings.Add(employeeTeamMapping);
+                    }
+                }
+
+                // Проверка и добавяне на мапинг на JobDoneTeamMapping
+                foreach (var jobDoneId in mapping.JobDoneIds)
+                {
+                    if (!context.JobDoneTeamMappings.Any(x => x.JobDoneId == jobDoneId && x.TeamId == mapping.TeamId))
+                    {
+                        var jobDoneTeamMapping = new JobDoneTeamMapping
+                        {
+                            JobDoneId = jobDoneId,
+                            TeamId = mapping.TeamId
+                        };
+                        context.JobDoneTeamMappings.Add(jobDoneTeamMapping);
+                    }
+                }
+            }
+
             context.SaveChanges();
         }
     }
---
+++ ElProApp/ElProApp.Data/SeedData/MappingData.cs
@@ -0,0 +1,18 @@
+﻿namespace ElProApp.Data.SeedData
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MappingData
+    {
+        public List<Mapping> Mappings { get; set; } = [];
+    }
+
+    public class Mapping
+    {
+        public Guid TeamId { get; set; }
+        public List<Guid> BuildingIds { get; set; } = [];
+        public List<Guid> EmployeeIds { get; set; } = [];
+        public List<Guid> JobDoneIds { get; set; } = [];
+    }
+}
