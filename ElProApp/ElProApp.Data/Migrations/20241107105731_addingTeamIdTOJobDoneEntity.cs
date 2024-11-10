cc14d24eccd845a1399e0ee56bdf6800031d9b19
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-11-10 10:34:53 +0200

    migration to add Id to JobDoneTeamMpping

---
+++ ElProApp/ElProApp.Data/Migrations/20241107105731_addingTeamIdTOJobDoneEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElProApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingTeamIdTOJobDoneEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TeamId",
+                table: "JobsDone",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                comment: "Foreign key for the team responsible for completing the job.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "JobsDone");
+        }
+    }
+}
