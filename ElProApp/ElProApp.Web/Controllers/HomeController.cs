2595419e6cd785c991786a0fb182d040b0d2e7ad
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-11-25 23:01:03 +0200

    fixing error handling

--- ElProApp/ElProApp.Web/Controllers/HomeController.cs
+++ ElProApp/ElProApp.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ namespace ElProApp.Web.Controllers
     using System.Diagnostics;
     using ElProApp.Web.Models;
     using Microsoft.AspNetCore.Http;
+    using AspNetCoreGeneratedDocument;
 
     public class HomeController : Controller
     {
@@ -27,63 +28,21 @@ namespace ElProApp.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int? statusCode = null)
         {
-            statusCode ??= HttpContext.Response.StatusCode;
+            if(!statusCode.HasValue) return View();
 
             switch (statusCode)
             {
                 case 404:
-                    return Handle404();
+                    return View("_Error404");
                 case 500:
-                    return Handle500();
+                    return View("_Error500");
                 case 403:
-                    return Handle403();
+                    return View("_Error403");
                 default:
-                    return HandleGenericError();
+                    return View("Error");
             }
         }
 
-        private IActionResult Handle404()
-        {
-            var errorModel = new ErrorViewModel
-            {
-                ErrorMessage = "Sorry, the page you are looking for could not be found.",
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            };
-
-            return View("_Error404", errorModel); 
-        }
-
-        private IActionResult Handle500()
-        {
-            var errorModel = new ErrorViewModel
-            {
-                ErrorMessage = "An internal server error occurred.",
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            };
-
-            return View("_Error500", errorModel);
-        }
-
-        private IActionResult Handle403()
-        {
-            var errorModel = new ErrorViewModel
-            {
-                ErrorMessage = "You do not have permission to view this page.",
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            };
-
-            return View("_Error403", errorModel);
-        }
-
-        private IActionResult HandleGenericError()
-        {
-            var errorModel = new ErrorViewModel
-            {
-                ErrorMessage = "An unexpected error occurred.",
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            };
-
-            return View("Error", errorModel);
-        }
+        
     }
 }
