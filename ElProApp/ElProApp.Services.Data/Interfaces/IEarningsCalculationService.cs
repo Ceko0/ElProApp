1f8ace5aef834e2d7e49d4859f5ecb0617c3ce53
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-12-01 12:51:00 +0200

    add money calculator

---
+++ ElProApp/ElProApp.Services.Data/Interfaces/IEarningsCalculationService.cs
@@ -0,0 +1,9 @@
+﻿using ElProApp.Web.Models.JobDone;
+
+namespace ElProApp.Services.Data.Interfaces
+{
+    public interface IEarningsCalculationService
+    {
+        public Task<bool> CalculateMoneyAsync(JobDoneInputModel model);
+    }
+}
